fd23d621b61e01469ad19aa02a2a3488af10ca2e
badrkb <badr.kbitat@gmail.com>
2016-10-07 22:14:19 +0200

    Add files via upload

---
+++ fonction map et reduce.js
@@ -0,0 +1,27 @@
+
+var listaMapper = ["bonjour", "le", "monde", "entier","monde","vous", "allez", "bien"]; //on commence par la création d'une liste à mapper 
+
+function map(listaMapper){
+
+  var listaShuffler= []; //on initialise la liste qu'on va appliquer sur elle la procédure shuffle and sort 
+
+  for(i = 0; i < lstmapp.length; i++){
+      listaReduire.push(lstmapp[i]);
+  }
+  //elle introduit chaque mot dans une liste clé valeur 
+
+  return listaShuffler;
+}
+
+var listaReduire = [{"bonjour", 1}, {"le", 1}, {"monde", 1}, {"entier", 1}, {"monde", 1}, {"vous", 1}, {"allez", 1}, {"bien", 1}];
+
+function reduce(listaReduire){
+
+  var listfinale = [];
+
+  for(i = 0; i < lstreduire.length; i++){//apres l'operation shuffle and sort on calcule le nombre de mot qu'on a dans la liste initiale
+
+		listaShuffler.push(lstreduire[i]);
+  }
+	return listfinale;
+}
